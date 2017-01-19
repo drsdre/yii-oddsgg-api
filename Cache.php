ac05a3c09ada0b8ab77301d025ae730e050e8a0d
Drsdre <github.com@drsdre.org>
2017-01-19 14:25:27 +0100

    Add statistics for failed API calls. Removed hinted function variables to stay compatible with PHP < 7.0.

--- Cache.php
+++ Cache.php
@@ -30,12 +30,18 @@ use drsdre\OddsGG\models\OddsGGTournament;
  */
 class Cache {
 
-	/** @var Client  $_client */
+	/** @var Client $_client OddsGG Client*/
 	protected $_client;
 
+	/** @var array $_stats statistics data */
 	protected $_stats = [];
 
-	public function __construct($client) {
+	/**
+	 * Cache constructor.
+	 *
+	 * @param $client
+	 */
+	public function __construct( $client ) {
 		$this->_client = $client;
 	}
 
@@ -68,18 +74,16 @@ class Cache {
 	 * @return bool
 	 */
 	public function updateAll( $init = false ) {
-		// Mark time when update starts (make sure server time is set to UTC)
-		$update_start_time = time();
-		$result            = true;
+		$result = true;
 
-		if ( ! $this->updateSports($init) ) {
+		if ( ! $this->updateSports( $init ) ) {
 			$result = false;
 		}
 
 		// Get data for all sports
 		foreach ( OddsGGSport::find()->all() as $Sport ) {
 
-			if ( ! $this->updateLeagueTournamentsBySport($Sport->id, $init) ) {
+			if ( ! $this->updateLeagueTournamentsBySport( $Sport->id, $init ) ) {
 				$result = false;
 			}
 
@@ -141,11 +145,11 @@ class Cache {
 	/**
 	 * Update Sports
 	 *
-	 * @param $bool init
+	 * @param bool $init
 	 *
-	 * @return bool if update succesfull
+	 * @return bool true if update successful
 	 */
-	public function updateSports( bool $init = false ) {
+	public function updateSports( $init = false ) {
 		// Get the data from API
 		$SportsResponse = $this->_client
 			->get( 'Sports', [], [ 'isUpdate' => $init == true ? 'false' : 'true' ] )
@@ -154,6 +158,8 @@ class Cache {
 
 		// Check if API response is valid
 		if ( ! $SportsResponse->isOk ) {
+			$this->incStat( 'api_sports_error_' . $SportsResponse->getErrorCode() );
+
 			return false;
 		}
 
@@ -176,7 +182,7 @@ class Cache {
 	 *
 	 * @return bool if update succesfull
 	 */
-	public function updateLeagueTournamentsBySport( int $SportId, bool $init = false ) {
+	public function updateLeagueTournamentsBySport( $SportId, $init = false ) {
 		// Get the data from API
 		$LeaguesResponse = $this->_client
 			->get( 'Leagues/Sport/' . $SportId, [], [ 'isUpdate' => $init == true ? 'false' : 'true' ] )
@@ -185,6 +191,8 @@ class Cache {
 
 		// Check if API response is valid
 		if ( ! $LeaguesResponse->isOk ) {
+			$this->incStat( 'api_Leagues/Sport_error_' . $LeaguesResponse->getErrorCode() );
+
 			return false;
 		}
 
@@ -196,12 +204,12 @@ class Cache {
 	/**
 	 * Update League Tournaments by Tournament from API
 	 *
-	 * @param int $SportId
+	 * @param int $TournamentId
 	 * @param bool $init
 	 *
-	 * @return bool if update succesfull
+	 * @return bool if update successful
 	 */
-	public function updateLeagueTournamentsByTournament( int $TournamentId, bool $init = false ) {
+	public function updateLeagueTournamentsByTournament( $TournamentId, $init = false ) {
 		// Get the data from API
 		$LeaguesResponse = $this->_client
 			->get( 'Leagues/Tournaments/' . $TournamentId, [], [ 'isUpdate' => $init == true ? 'false' : 'true' ] )
@@ -210,6 +218,8 @@ class Cache {
 
 		// Check if API response is valid
 		if ( ! $LeaguesResponse->isOk ) {
+			$this->incStat( 'api_Leagues/Tournaments_error' );
+
 			return false;
 		}
 
@@ -229,6 +239,7 @@ class Cache {
 				$league['Id'],
 				$league
 			);
+
 			// If upsert not okay, skip further processing of this record
 			if ( ! $this->checkUpsert( $OddsGGLeague ) ) {
 				continue;
@@ -257,7 +268,7 @@ class Cache {
 	 *
 	 * @return bool if update succesfull
 	 */
-	public function updateMatchesBySport( int $SportId, bool $init = false ) {
+	public function updateMatchesBySport( $SportId, $init = false ) {
 		// Get the data from API
 		$MatchesResponse = $this->_client
 			->get( 'Matches/Sport/' . $SportId, [], [ 'isUpdate' => $init == true ? 'false' : 'true' ] )
@@ -266,6 +277,8 @@ class Cache {
 
 		// Check if API response is valid
 		if ( ! $MatchesResponse->isOk ) {
+			$this->incStat( 'api_Matches/Sport_error_' . $MatchesResponse->getErrorCode() );
+
 			return false;
 		}
 
@@ -315,7 +328,7 @@ class Cache {
 			if (
 				count( $errors ) == 1 &&
 				isset( $errors['TournamentId'] ) &&
-				array_search('Tournament is invalid.', $errors['TournamentId']) !== false
+				array_search( 'Tournament is invalid.', $errors['TournamentId'] ) !== false
 			) {
 				// Clear tournment ID
 				$match['TournamentId'] = null;
@@ -337,7 +350,7 @@ class Cache {
 	 *
 	 * @return bool if update succesfull
 	 */
-	public function updateMarketsBySport( int $SportId, bool $init = false ) {
+	public function updateMarketsBySport( $SportId, $init = false ) {
 
 		// Get the data from API
 		$MarketsResponse = $this->_client
@@ -347,6 +360,8 @@ class Cache {
 
 		// Check if API response is valid
 		if ( ! $MarketsResponse->isOk ) {
+			$this->incStat( 'api_Markets/Sport_error_' . $MarketsResponse->getErrorCode() );
+
 			return false;
 		}
 
@@ -358,12 +373,12 @@ class Cache {
 	/**
 	 * Update Markets by League from API
 	 *
-	 * @param int $SportId
+	 * @param int $LeagueId
 	 * @param bool $init
 	 *
 	 * @return bool if update succesfull
 	 */
-	public function updateMarketsByLeague( int $LeagueId, bool $init = false ) {
+	public function updateMarketsByLeague( $LeagueId, $init = false ) {
 
 		// Get the data from API
 		$MarketsResponse = $this->_client
@@ -373,6 +388,8 @@ class Cache {
 
 		// Check if API response is valid
 		if ( ! $MarketsResponse->isOk ) {
+			$this->incStat( 'api_Markets/Categories_error_' . $MarketsResponse->getErrorCode() );
+
 			return false;
 		}
 
@@ -402,7 +419,7 @@ class Cache {
 			// Store the odds
 			foreach ( $market['Odds'] as $odd ) {
 				$odd['Timestamp'] = strtotime( $odd['Timestamp'] );
-				$odd['Value'] = (string) $odd['Value'];
+				$odd['Value']     = (string) $odd['Value'];
 				$OddsGGOdd        = OddsGGOdd::upsert(
 					$odd['Id'],
 					$odd
